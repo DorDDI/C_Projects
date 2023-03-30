8b982b9a913eb32b46f9760fbb9015208d8bd0e2
DDI112 DDI112 <dorlasry71@gmail.com>
2023-03-30 16:10:15 +0300

    update and add funcs

--- Files functions/files.c
+++ Files functions/files.c
@@ -138,3 +138,60 @@ res2 = fscanf(second_file, "%c%*c", &active2);
 	fclose(series_file);
 }
 
+serie** getUnifyDatabaseDetails(int* counter)
+//build and save the details of the series text file to the memory
+{
+	int i;
+	FILE* series_file;									//pointer for the series file
+	series_file = fopen("series.txt", "r");
+
+	int count_series = 0;								//check how many series there is
+	int res = fscanf(series_file, "%*[^\n]%*c");		
+	while (res != EOF)									//run in all the series file
+	{
+		count_series++;									//add to the series counter
+		res = fscanf(series_file, "%*[^\n]%*c");
+	}
+	serie** series_list;
+	series_list = (serie**)malloc(count_series * sizeof(serie*));		//create a dinamic arry of pointers to serie struct in len of the number of series
+	if (series_list == NULL)
+		exit(0);
+	rewind(series_file);								//return to the top line in the file
+	res = 0;
+	*counter = count_series;							//save the number if series in the main
+	for (i = 0; i < count_series; i++)					//run on all the series
+	{
+		char name[20] = { 0 };							
+		int seria_code;
+		int seria_seasons;
+		res = fscanf(series_file, "%[^,]%*c", name);
+		res = fscanf(series_file, "%d%*c", &seria_code);
+		res = fscanf(series_file, " %d%*c", &seria_seasons);
+
+		series_list[i] = (serie*)malloc(sizeof(serie));		//create a pointer to serie arry that we will be able to save it after the function is over
+		if (series_list[i] == NULL)
+			exit(0);
+		series_list[i]->numSeasons = seria_seasons;			//save the data from the file to the struct: serie number of seasons
+		series_list[i]->rank = 0;							//serie rank will start with 0
+		series_list[i]->serieCode = seria_code;				//the serie code
+		strcpy(series_list[i]->serieName, name);			//the serie name
+		series_list[i]->watchingDetails = (int*)calloc(seria_seasons ,sizeof(int));	//create a dinamic int list of the watching list in len of the seasons number
+	}
+
+	FILE* watching_create;
+	watching_create = fopen("watching.txt", "w");			//create the watching txt file - open and close it
+	fclose(watching_create);
+	return series_list;
+}
+
+void printSeriesList(serie** watch_series, int index)
+//print the datails of every serie
+{
+	int k;
+	printf("%03d-%s\n", watch_series[index]->serieCode, watch_series[index]->serieName);  //print the serie code for a specific serie
+	for (k = 0; k < watch_series[index]->numSeasons; k++)
+	{
+		printf("Season %d:%d\n", k + 1, (watch_series[index]->watchingDetails)[k]);		  //print the serie whatchin list for a specific serie
+	}
+}
+
