9fe3f8fe8c73b9faf9858153a83c2af6ae4b11d1
DDI112 DDI112 <dorlasry71@gmail.com>
2023-03-30 16:14:21 +0300

    crete the menu

---
+++ String functions/Strings.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+	
+	return 0;
+}
+
+void printMenu()
+//print the start menu 
+{
+	printf("The menu:\n");
+	printf("=========\n");
+	printf("0. Exit\n");
+	printf("1. Insert words\n");
+	printf("2. Print words\n");
+	printf("3. Sort words lexicographic\n");
+	printf("4. Sort words by length\n");
+	printf("5. Sort words by ascii sum\n");
+	printf("6. Sort words by ascii average\n");
+	printf("7. Sort each word by ascii\n");
+	printf("8. Sort all words as one\n");
+}
