9e00635e559f5a1873f1b80f6c3b3bcee42c7fc5
DDI112 DDI112 <dorlasry71@gmail.com>
2023-03-30 16:14:53 +0300

    add 2 funcs

--- String functions/Strings.c
+++ String functions/Strings.c
@@ -24,3 +24,49 @@ void printMenu()
 	printf("7. Sort each word by ascii\n");
 	printf("8. Sort all words as one\n");
 }
+
+int checkString(char str1[])
+//input a string check if the string is insert properly and return it
+{
+	char str_new[200] = {0};
+	gets(str_new);					//input the string
+	int i;
+	int flag = 1;
+	if (strlen(str_new) > 19 || strlen(str_new) < 1)    //check if the string is insert properly 
+		return 0;
+	for (i = 0; i < strlen(str_new); i++)		//properly by length	
+	{
+		if (!((str_new[i] >= 'a' && str_new[i] <= 'z') || (str_new[i] >= 'A' && str_new[i] <= 'Z')))  //properly by chars
+			return 0;
+	}
+	strcpy(str1 , str_new); //if insert properly, copy the input string to the send string
+	return 1;
+
+}
+
+void printWords(char list_words[][20], int words_num) 
+//print the words in the word list with ascii sum, ascii average and the word itself
+{
+	int i;
+	for (i = 0; i < words_num; i++)
+	{
+		char sum_word[5];										//sum of space in the ascii sum print 
+		stringReturn(sum_word, asciiSum(list_words[i]));		
+		char avg_word[2];										//sum of space in the ascii avg print 
+		if (asciiAvg(list_words[i]) >= 100)						//check if the avg number is 3 digits
+		{	
+			avg_word[0] = '\0';
+		}
+		else                                                    //if the avg number is 2 digits
+		{
+			avg_word[0] = ' ';
+			avg_word[1] = '\0';
+		}
+			
+		if (i < 9)
+			printf("0%d:(ascii sum%s%d, ascii avg %s%.2f)%s\n", i + 1, sum_word, asciiSum(list_words[i]), avg_word, asciiAvg(list_words[i]), list_words[i]); //print the string from 0 to 9 if needed
+		else
+			printf("%d:(ascii sum%s%d, ascii avg %s%.2f)%s\n", i + 1, sum_word, asciiSum(list_words[i]), avg_word, asciiAvg(list_words[i]), list_words[i]); //print the 10th string if needed
+	}
+}
+
